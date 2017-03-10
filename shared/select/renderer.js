58b13e92fde91d81b6c7a851d636224f9982fdac
mixtern <mixternnik@gmail.com>
2017-03-10 15:05:00 +0000

    making server form, editing select/renderer.js

--- shared/select/renderer.js
+++ shared/select/renderer.js
@@ -1,1 +1,6 @@
-
+;function refresh(){
+    var tst = document.getElementById("tst"),
+        lvl = document.getElementById("lvl"),
+        tstInp = document.getElementById("tst-inp"),
+        lvlInp = document.getElementById("lvl-inp");
+};
